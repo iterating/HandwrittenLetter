a196823cdc8940c3b3694b7f80eaf68c2808444e
Jonathan Young <jy>
2024-11-08 03:51:06 -0800

    error handling

--- handwrite.py
+++ handwrite.py
@@ -1,3 +1,9 @@
+import sys
+import os
+import pygame
+from pygame.locals import *
+from pygame import Color
+from PIL import Image, ImageEnhance
 
 # Constants
 WIDTH = 500
@@ -7,28 +13,53 @@ DRAW_HEIGHT = 300
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
-
-"""
-The main file for the Hand Writing project
-
-This program is a simple tool for creating hand writing datasets.
-"""
-
-
-import sys
-import pygame
-from pygame.locals import *
-from pygame import Color
-import os
-from PIL import Image, ImageEnhance
-
-
+# Initialize Pygame
 try:
     pygame.init()
 except pygame.error as e:
-    print("Error: {}".format(e))
+    print(f"Error: {e}")
     sys.exit()
 
+# Set up the display
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+screen.fill(WHITE)
+pygame.display.set_caption("Handwritten")
+
+# Create a drawing surface
+mydraw = pygame.Surface((DRAW_WIDTH, DRAW_HEIGHT), pygame.SRCALPHA)
+
+# Program label
+font = pygame.font.Font("freesansbold.ttf", 18)
+text = font.render("Text To Hand Writing", True, (62, 97, 219))
+screen.blit(text, (260, 30))
+
+# Button coordinates and colors
+createButton = (220, 65, 250, 35)
+clearButton = (220, 110, 250, 35)
+newSetButton = (220, 155, 250, 35)
+noactcolor = (164, 170, 164)
+actcolor = (184, 238, 57)
+empty = Color(0, 0, 0, 0)
+
+# Text coordinates
+t1 = (320, 75)
+t2 = (320, 120)
+t3 = (300, 165)
+t4 = (100, 320)
+textCoords = (65, 320)
+instructions = "Draw --> A"
+
+# Text font size and style
+txtfont = pygame.font.Font("freesansbold.ttf", 15)
+txt1 = txtfont.render("Create", True, BLACK)
+txt2 = txtfont.render("Clear", True, BLACK)
+txt3 = txtfont.render("Create New Set", True, BLACK)
+
+# Setup initial display
+screen.blit(mydraw, (10, 10))
+brush = pygame.transform.scale(pygame.image.load("images/brush.png"), (15, 15))
+pygame.display.update()
+clock = pygame.time.Clock()
 
 # Variables
 z = 0
@@ -38,150 +69,70 @@ actset = 0
 letter_act_index = 0
 letterlist = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789,.?{}()-_+=*&^%@<>|/'\"\\;:"
 
-
-# Set up the display
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-screen.fill(WHITE)
-pygame.display.set_caption("Handwritten")
-
-mydraw = pygame.Surface((DRAW_WIDTH, DRAW_HEIGHT), pygame.SRCALPHA)
-
-# Program label
-font = pygame.font.Font("freesansbold.ttf", 18)
-text = font.render("Text To Hand Writing", True, (62, 97, 219))
-screen.blit(text, (260, 30))
-
-# button coordinates
-createButton = (220, 65, 250, 35)
-clearButton = (220, 110, 250, 35)
-newSetButton = (220, 155, 250, 35)
-
-# button color
-noactcolor = (164, 170, 164)
-actcolor = (184, 238, 57)
-empty = Color(0, 0, 0, 0)
-
-# text coordinates
-t1 = (320, 75)
-t2 = (320, 120)
-t3 = (300, 165)
-
-# Error label
-t4 = (100, 320)
-textCoords = (65, 320)
-instructions = "Draw --> A"
-font1 = pygame.font.Font("freesansbold.ttf", 20)
-try:
-    textbox = font1.render(instructions, True, BLACK)
-except pygame.error as e:
-    print("Error: {}".format(e))
-    sys.exit()
-
-screen.blit(textbox, textCoords)
-
-# text font size/style
-txtfont = pygame.font.Font("freesansbold.ttf", 15)
-txt1 = txtfont.render("Create", True, BLACK)
-txt2 = txtfont.render("Clear", True, BLACK)
-txt3 = txtfont.render("Create New Set", True, BLACK)
-
-
-screen.blit(mydraw, (10, 10))
-
-brush = pygame.image.load("images/brush.png")
-brush = pygame.transform.scale(brush, (15, 15))
-
-pygame.display.update()
-clock = pygame.time.Clock()
-
-
 def render_display():
-    """
-    Display the current instruction on the screen
-    """
+    """Display the current instruction on the screen"""
     screen.fill(WHITE, (65, 320, 120, 100))
-    textbox = font1.render(instructions, True, BLACK)
+    textbox = font.render(instructions, True, BLACK)
     screen.blit(textbox, textCoords)
+
 def check_directory():
-    """
-    Check if the directory exists and create it if it doesn't
-    """
-    global actdirectory
+    """Check if the directory exists and create it if it doesn't"""
+    global actdirectory, no_set, actset
     sdir = os.path.isdir(actdirectory)
     if sdir:
         cdir = os.listdir("images/letters")
         no_set = len(cdir)
         actset = no_set + 1
-        actdirectory = "images/letters/set{}".format(actset)
+        actdirectory = f"images/letters/set{actset}"
         os.mkdir(actdirectory)
         os.mkdir(actdirectory + "/blue")
         os.mkdir(actdirectory + "/black")
     else:
         os.mkdir("images/letters")
         actset = 1
-        actdirectory = "images/letters/set{}".format(actset)
+        actdirectory = f"images/letters/set{actset}"
         os.mkdir(actdirectory)
         os.mkdir(actdirectory + "/blue")
         os.mkdir(actdirectory + "/black")
 
-
 def create_letter():
-    """
-    Save the current letter as a PNG image and create a new letter
-    """
-    global actdirectory
-    global letter_act_index
-    global instructions
+    """Save the current letter as a PNG image and create a new letter"""
+    global actdirectory, letter_act_index, instructions
     letter = ord(letterlist[letter_act_index])
-    imfile = "{}/blue/{}.png".format(actdirectory, letter)
-    # Blue Letter
+    imfile = f"{actdirectory}/blue/{letter}.png"
     pygame.image.save(mydraw, imfile)
-
-    # black letter
     im = Image.open(imfile)
     enhancer = ImageEnhance.Brightness(im)
     factor = 0.35
     im_output = enhancer.enhance(factor)
-    im_output.save("{}/black/{}.png".format(actdirectory, letter))
+    im_output.save(f"{actdirectory}/black/{letter}.png")
     letter_act_index += 1
     if letter_act_index < 87:
-        instructions = "Draw --> {}".format(letterlist[letter_act_index])
+        instructions = f"Draw --> {letterlist[letter_act_index]}"
     else:
         instructions = "Set Done."
         btn_reset()
 
-
 def reset_surface():
-    """
-    Reset the drawing surface
-    """
+    """Reset the drawing surface"""
     pygame.draw.rect(screen, WHITE, (10, 10, DRAW_WIDTH, DRAW_HEIGHT))
     mydraw.fill(empty)
     pygame.draw.line(screen, (163, 163, 163), (10, 110), (210, 110), 1)
     pygame.draw.line(screen, (163, 163, 163), (10, 210), (210, 210), 1)
     pygame.draw.rect(screen, BLACK, (10, 10, DRAW_WIDTH, DRAW_HEIGHT), 2)
 
-
 def btn_reset():
-    """
-    Reset the buttons
-    """
-    # draw buttons
+    """Reset the buttons"""
     pygame.draw.rect(screen, noactcolor, createButton)
     pygame.draw.rect(screen, noactcolor, clearButton)
     pygame.draw.rect(screen, noactcolor, newSetButton)
-
-    # write Text
     screen.blit(txt1, t1)
     screen.blit(txt2, t2)
     screen.blit(txt3, t3)
     pygame.display.update()
 
-
 def btn_clicked(btnid):
-    """
-    Change the color of the button to show which one is active
-    """
+    """Change the color of the button to show which one is active"""
     btn_reset()
     if btnid == 1:
         pygame.draw.rect(screen, actcolor, createButton)
@@ -194,14 +145,10 @@ def btn_clicked(btnid):
         screen.blit(txt3, t3)
     pygame.display.update()
 
-
 def mouse_clicked(x, y):
-    """
-    Check if the mouse is clicked on a button and perform the action
-    """
-    global letter_act_index
-    global instructions
-    if x > 220 and y > 65 and x < 470 and y < 100:
+    """Check if the mouse is clicked on a button and perform the action"""
+    global letter_act_index, instructions
+    if createButton[0] < x < createButton[0] + createButton[2] and createButton[1] < y < createButton[1] + createButton[3]:
         btn_clicked(1)
         if letter_act_index < 87:
             create_letter()
@@ -210,16 +157,15 @@ def mouse_clicked(x, y):
             btn_reset()
         reset_surface()
         render_display()
-    elif x > 220 and y > 110 and x < 470 and y < 145:
+    elif clearButton[0] < x < clearButton[0] + clearButton[2] and clearButton[1] < y < clearButton[1] + clearButton[3]:
         btn_clicked(2)
         reset_surface()
-    elif x > 220 and y > 155 and x < 470 and y < 190:
+    elif newSetButton[0] < x < newSetButton[0] + newSetButton[2] and newSetButton[1] < y < newSetButton[1] + newSetButton[3]:
         btn_clicked(3)
         check_directory()
         instructions = "Draw --> A"
         letter_act_index = 0
         render_display()
-
 
 check_directory()
 reset_surface()
@@ -244,4 +190,3 @@ while True:
             mydraw.blit(brush, (x - 14, y - 14))
             screen.blit(mydraw, (10, 10))
             pygame.display.update()
-
--- handwriteRender.py
+++ handwriteRender.py
@@ -13,22 +13,24 @@ The output of the script is written to a file named output.html.
 import random
 import os
 
+LETTER_SETS_DIR = "images/letters"
+BACKGROUND_IMAGE = "images/background.png"
+OUTPUT_FILE = "output.html"
+INPUT_FILE = "input.txt"
 
-current_letter_color = "blue"
-current_letter_set = "set0"
-toggle_letter_color = False
-total_letter_sets = len(os.listdir("images/letters")) + 1
+LETTER_COLORS = ("blue", "black")
+CHARACTER_HEIGHT = 25
+CHARACTER_WIDTH = 15
+CHARACTER_MARGIN_TOP = 5
+CHARACTER_MARGIN_BOTTOM = 10
 
 html_content = [
     "<html>",
     "<head>",
     "<style>",
-    # set the background image for the output
     ".lines { width: 100%; height: auto; float: left; }",
-    "#paper { background: white; background-image: url('images/background.png'); height: auto; float: left; padding: 50px 50px; width: 90%; }",
-    # set the size of the characters
-    "img, span { height: 25px; width: 15px; float: left; margin-top: 5px; margin-bottom: 10px; }",
-    # set the brightness of the characters
+    f"#paper {{ background: white; background-image: url('{BACKGROUND_IMAGE}'); height: auto; float: left; padding: 50px 50px; width: 90%; }}",
+    f"img, span {{ height: {CHARACTER_HEIGHT}px; width: {CHARACTER_WIDTH}px; float: left; margin-top: {CHARACTER_MARGIN_TOP}px; margin-bottom: {CHARACTER_MARGIN_BOTTOM}px; }}",
     ".clblack { filter: brightness(30%); }",
     ".clblue { filter: brightness(100%); }",
     "</style>",
@@ -37,44 +39,29 @@ html_content = [
     "<div id='paper'></div>"
 ]
 
-
 try:
-    with open("input.txt", "r") as input_file:
-        for line in input_file:
-            # strips the newline character
+    with open(INPUT_FILE, "r") as file:
+        for line in file:
             current_string = line.strip()
             html_content.append('<div class="lines">')
+            current_letter_color = "blue"
+            toggle_letter_color = False
             for character in current_string:
-                # get char ASCII Code of char
                 character_code = ord(character)
-
-                # select Random set
-                random_letter_set = random.randrange(1, total_letter_sets)
-                current_letter_set = f"set{random_letter_set}"
-
-                if character_code == 35:
-                    # toggle color of characters every time the user types a '#'
-                    if toggle_letter_color:
-                        current_letter_color = "blue"
-                        toggle_letter_color = False
-                    else:
-                        current_letter_color = "black"
-                        toggle_letter_color = True
-                elif character_code == 32 or character_code == 36:
+                if character_code == 35:  # '#'
+                    current_letter_color = LETTER_COLORS[toggle_letter_color]
+                    toggle_letter_color = not toggle_letter_color
+                elif character_code == 32 or character_code == 36:  # ' ' or '$'
                     html_content.append("<span></span>")
                 else:
-                    html_content.append(
-                        f"<img src='images/letters/{current_letter_set}/{current_letter_color}/{character_code}.png'/>"
-                    )
+                    current_letter_set = f"set{random.randrange(1, len(os.listdir(LETTER_SETS_DIR)) + 1)}"
+                    html_content.append(f"<img src='{LETTER_SETS_DIR}/{current_letter_set}/{current_letter_color}/{character_code}.png'/>")
             html_content.append("</div>")
-
     html_content.append("</div></body></html>")
 
-    with open("output.html", "w") as output_html:
-        output_html.writelines(html_content)
+    with open(OUTPUT_FILE, "w") as file:
+        file.writelines(html_content)
 except FileNotFoundError:
-    print("Error: input.txt not found.")
+    print(f"Error: {INPUT_FILE} not found.")
 except Exception as e:
     print(f"An error occurred: {e}")
-
-
