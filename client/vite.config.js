20d0927fd74259c90cc30a6f230e0a137b968b83
Jonathan Young <jy>
2025-01-04 07:27:30 -0800

    dist

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
@@ -12,7 +13,13 @@ export default defineConfig({
     }
   },
   build: {
+    outDir: 'dist',
     emptyOutDir: true,
-    assetsDir: 'assets'
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html')
+      }
+    }
   }
 })
